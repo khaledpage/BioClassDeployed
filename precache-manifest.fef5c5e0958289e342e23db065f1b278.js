7c764bfa72a0ab1e666b1c40e7b216fb71fa6061
khaledpage <44144104+khaledpage@users.noreply.github.com>
2020-03-04 20:20:44 +0100

    n

---
+++ precache-manifest.fef5c5e0958289e342e23db065f1b278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6251fd82611e3e3afc12117310abc5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c7641e27704b0d20651",
+    "url": "/static/css/main.d5b4ab5a.chunk.css"
+  },
+  {
+    "revision": "6c242f16c30552ef74c5",
+    "url": "/static/js/2.4c785919.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4c785919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7641e27704b0d20651",
+    "url": "/static/js/main.11d79169.chunk.js"
+  },
+  {
+    "revision": "8245774d9856c1eaa5a3",
+    "url": "/static/js/runtime-main.000f302a.js"
+  }
+]);
